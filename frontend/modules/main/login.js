85fac15c1aa513bfa5c9221ff06f5e25e8dcb809
kentpan <ayue222@126.com>
2016-10-12 16:38:50 +0800

    complete 70%

--- frontend/modules/main/login.js
+++ frontend/modules/main/login.js
@@ -1,4 +1,4 @@
-var mba = angular.module('MBA', []);
+var mba = angular.module('xunbao', []);
 /**
  * Login Controller
  */
@@ -7,12 +7,4 @@ mba.controller('LoginCtrl', function ($rootScope, $scope) {
     $scope.year = new Date().getFullYear();
     var path = location.pathname.match(/^.*\//);
     // $scope.uuap_callback_url = "http://" + location.host + path + 'run.html#/index';
-    $scope.uuap_callback_url = "http://" + location.host + '/searchboxbi/api/login';
-    $scope.goRun = function ($event) {
-        if (!!/(kent|tianbin|qiuzhiqun|luoaihua)\.baidu\.com/i.test(location.host)) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            location.replace('./run.html#/index');
-        }
-    };
 });
